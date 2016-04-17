bf077c086cc3050170f9b0c7b16d2a22650db66e
Chin-Jung Hsu <chsu6@ncsu.edu>
2016-04-17 10:41:45 -0400

    The new results.

--- driver.sh
+++ driver.sh
@@ -1,11 +1,11 @@
-python pom3a.py &
-python pom3b.py &
-python pom3c.py &
-python pom3d.py &
-python xomo_flight.py &
-python xomo_ground.py &
-python xomo_osp.py &
-python xomo_osp2.py &
+python pom3a.py &> pom3a.log &
+python pom3b.py &> pom3b.log &
+python pom3c.py &> pom3c.log &
+python pom3d.py &> pom3d.log &
+python xomo_flight.py &> xomo_flight.log &
+python xomo_ground.py &> xomo_ground.log &
+python xomo_osp.py &> xomo_osp.log &
+python xomo_osp2.py &> xomo_osp2.log &
 
 wait
 
--- jmoo_properties.py
+++ jmoo_properties.py
@@ -90,7 +90,7 @@ build_new_pop = False                                       # Whether or not to 
 Configurations = {
     "Universal": {
         "Repeats" : 20,
-        "Population_Size" : 100,
+        "Population_Size" : 10000,
         "No_of_Generations" : 1
     },
     "NSGAIII": {
