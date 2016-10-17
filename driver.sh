4276c95f95d56cc93be4ba279804d6ece465e768
Vivek Nair <vivekaxl@gmail.com>
2016-10-17 15:18:41 -0400

    modify driver.sh

--- driver.sh
+++ driver.sh
@@ -1,11 +1,38 @@
-python pom3a.py &
-python pom3b.py &
-python pom3c.py &
-python pom3d.py &
-python xomo_flight.py &
-python xomo_ground.py &
-python xomo_osp.py &
-python xomo_osp2.py &
+#!/usr/bin/env bash
+python monrp1_8192.py &
+python monrp2_8192.py &
+python monrp3_8192.py &
+python monrp4_8192.py &
+python nrp1_8192.py &
+python nrp2_8192.py &
+python nrp3_8192.py &
+python nrp4_8192.py &
+python pom3a_8192.py &
+python pom3b_8192.py &
+python pom3c_8192.py &
+python pom3d_8192.py &
+python xomo_all_8192.py &
+python xomo_flight_8192.py &
+python xomo_ground_8192.py &
+python xomo_osp2_8192.py &
+python xomo_osp_8192.py &
+python monrp1_10000.py &
+python monrp2_10000.py &
+python monrp3_10000.py &
+python monrp4_10000.py &
+python nrp1_10000.py &
+python nrp2_10000.py &
+python nrp3_10000.py &
+python nrp4_10000.py &
+python pom3a_10000.py &
+python pom3b_10000.py &
+python pom3c_10000.py &
+python pom3d_10000.py &
+python xomo_all_10000.py &
+python xomo_flight_10000.py &
+python xomo_ground_10000.py &
+python xomo_osp2_10000.py &
+python xomo_osp_10000.py &
 
 wait
 
