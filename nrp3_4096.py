f2f2a973d13d988d7e52f19d8762c218151f19ca
Vivek Nair <vivekaxl@gmail.com>
2016-10-19 23:04:58 -0400

    added only monrp

---
+++ nrp3_4096.py
@@ -0,0 +1,18 @@
+from jmoo_properties import *
+from jmoo_core import *
+
+# Wrap the tests in the jmoo core framework
+tests = jmoo_test([MONRP(50, 4, 5, 4, 90)], algorithms)
+reports = None
+
+local_configurations = {
+    "Universal": {
+        "Repeats" : 20,
+        "Population_Size" : 4096,
+        "No_of_Generations" : 1
+    },
+}
+
+# Associate core with tests and reports
+core = JMOO(tests, reports, local_configurations)
+core.doTests()
