f29412f79dd50b9a4761cefd42330250ff9dd4de
Jianfeng <ginfungchan@gmail.com>
2016-10-21 17:31:35 -0400

    varify XOMO decision spaces

--- Problems/XOMO/XOMO_all.py
+++ Problems/XOMO/XOMO_all.py
@@ -101,3 +101,276 @@ class XOMO_all(jmoo_problem):
 
     def evalConstraints(prob, input=None):
         return False  # no constraints
+
+
+class XOMO_allS(jmoo_problem):
+    "Xomo_all small version                                                                 "
+
+    def __init__(prob, percentage=-1):
+        prob.percentage=percentage
+        prob.name = "xomo_all(S)"
+        # Should be as xomol.names to maintain order of LOWs and UPs
+        names = ["aa", "sced", "cplx", "site", "resl", "acap", "etat", "rely",
+                 "Data", "prec", "pmat", "aexp", "flex", "pcon", "tool", "time",
+                 "stor", "docu", "b", "plex", "pcap", "kloc", "ltex", "pr",
+                 "ruse", "team", "pvol"]
+        # Generic Bounds as per menzies.us/pdf/06xomo101.pdf fig.9
+        bounds = {"aa": (1, 6),
+                  "sced": (1.00, 1.43),
+                  "cplx": (0.73, 1.74),
+                  "site": (0.80, 1.22),
+                  "resl": (1.41, 7.07),
+                  "acap": (0.71, 1.42),
+                  "etat": (1, 6),
+                  "rely": (0.82, 1.26),
+                  "Data": (0.90, 1.28),
+                  "prec": (1.24, 6.20),
+                  "pmat": (1.56, 7.80),
+                  "aexp": (0.81, 1.22),
+                  "flex": (1.01, 5.07),
+                  "pcon": (0.81, 1.29),
+                  "tool": (0.78, 1.17),
+                  "time": (1.00, 1.63),
+                  "stor": (1.00, 1.46),
+                  "docu": (0.81, 1.23),
+                  "b": (3, 10),
+                  "plex": (0.85, 1.19),
+                  "pcap": (0.76, 1.34),
+                  "kloc": (2, 1000),
+                  "ltex": (0.84, 1.20),
+                  "pr": (1, 6),
+                  "ruse": (0.95, 1.24),
+                  "team": (1.01, 5.48),
+                  "pvol": (0.87, 1.30)}
+        # bounds specific to all model
+        bounds_all = {"prec": (1.24, 6.2),
+                      "flex": (1.01, 5.07),
+                      "resl": (1.41, 7.07),
+                      "team": (1.01, 5.48),
+                      "pmat": (1.56, 7.8),
+                      "rely": (0.82, 1.26),
+                      "cplx": (0.73, 1.74),
+                      "Data": (0.9, 1.14),
+                      "ruse": (0.95, 1.24),
+                      "time": (1, 1.63),
+                      "stor": (1, 1.17),
+                      "pvol": (0.87, 1.3),
+                      "acap": (0.71, 1.19),
+                      "pcap": (0.76, 1),
+                      "pcon": (0.81, 1.29),
+                      "aexp": (0.81, 1.22),
+                      "plex": (0.91, 1.19),
+                      "ltex": (0.84, 1.2),
+                      "tool": (0.78, 1.09),
+                      "sced": (1, 1.43),
+                      "site": (0.8, 1.22),
+                      "docu": (0.81, 1.23),
+                      "kloc": (7, 418)}
+        # Update main bounds with bounds of all
+        for key, val in bounds_all.items():
+            delta = max(val) - min(val)
+            bounds[key] = (min(val)+delta*0.35, min(val)+delta*0.65)
+
+            if min(val) == max(val): bounds[key] = (min(val), max(val) + 0.000001)  # To remove division by 0
+
+        prob.decisions = [jmoo_decision(names[i], bounds[names[i]][0], bounds[names[i]][1]) for i in range(len(names))]
+
+        prob.objectives = [jmoo_objective("Effort", True), jmoo_objective("Months", True),
+                           jmoo_objective("Defects", True), jmoo_objective("Risks", True)]
+
+    def evaluate(prob, input=None):
+        if input:
+            for i, decision in enumerate(prob.decisions):
+                decision.value = input[i]
+        else:
+            input = [decision.value for decision in prob.decisions]
+        xomoxo = xomol()
+        output = xomoxo.run(input)
+        for i, objective in enumerate(prob.objectives):
+            objective.value = output[i]
+        return [objective.value for objective in prob.objectives]
+
+    def evalConstraints(prob, input=None):
+        return False  # no constraints
+
+
+class XOMO_allM(jmoo_problem):
+    "Xomo_all medium version                                                                 "
+
+    def __init__(prob, percentage=-1):
+        prob.percentage=percentage
+        prob.name = "xomo_all(M)"
+        # Should be as xomol.names to maintain order of LOWs and UPs
+        names = ["aa", "sced", "cplx", "site", "resl", "acap", "etat", "rely",
+                 "Data", "prec", "pmat", "aexp", "flex", "pcon", "tool", "time",
+                 "stor", "docu", "b", "plex", "pcap", "kloc", "ltex", "pr",
+                 "ruse", "team", "pvol"]
+        # Generic Bounds as per menzies.us/pdf/06xomo101.pdf fig.9
+        bounds = {"aa": (1, 6),
+                  "sced": (1.00, 1.43),
+                  "cplx": (0.73, 1.74),
+                  "site": (0.80, 1.22),
+                  "resl": (1.41, 7.07),
+                  "acap": (0.71, 1.42),
+                  "etat": (1, 6),
+                  "rely": (0.82, 1.26),
+                  "Data": (0.90, 1.28),
+                  "prec": (1.24, 6.20),
+                  "pmat": (1.56, 7.80),
+                  "aexp": (0.81, 1.22),
+                  "flex": (1.01, 5.07),
+                  "pcon": (0.81, 1.29),
+                  "tool": (0.78, 1.17),
+                  "time": (1.00, 1.63),
+                  "stor": (1.00, 1.46),
+                  "docu": (0.81, 1.23),
+                  "b": (3, 10),
+                  "plex": (0.85, 1.19),
+                  "pcap": (0.76, 1.34),
+                  "kloc": (2, 1000),
+                  "ltex": (0.84, 1.20),
+                  "pr": (1, 6),
+                  "ruse": (0.95, 1.24),
+                  "team": (1.01, 5.48),
+                  "pvol": (0.87, 1.30)}
+        # bounds specific to all model
+        bounds_all = {"prec": (1.24, 6.2),
+                      "flex": (1.01, 5.07),
+                      "resl": (1.41, 7.07),
+                      "team": (1.01, 5.48),
+                      "pmat": (1.56, 7.8),
+                      "rely": (0.82, 1.26),
+                      "cplx": (0.73, 1.74),
+                      "Data": (0.9, 1.14),
+                      "ruse": (0.95, 1.24),
+                      "time": (1, 1.63),
+                      "stor": (1, 1.17),
+                      "pvol": (0.87, 1.3),
+                      "acap": (0.71, 1.19),
+                      "pcap": (0.76, 1),
+                      "pcon": (0.81, 1.29),
+                      "aexp": (0.81, 1.22),
+                      "plex": (0.91, 1.19),
+                      "ltex": (0.84, 1.2),
+                      "tool": (0.78, 1.09),
+                      "sced": (1, 1.43),
+                      "site": (0.8, 1.22),
+                      "docu": (0.81, 1.23),
+                      "kloc": (7, 418)}
+        # Update main bounds with bounds of all
+        for key, val in bounds_all.items():
+            delta = max(val) - min(val)
+            bounds[key] = (min(val)+delta*0.25, min(val)+delta*0.75)
+
+            if min(val) == max(val): bounds[key] = (min(val), max(val) + 0.000001)  # To remove division by 0
+
+        prob.decisions = [jmoo_decision(names[i], bounds[names[i]][0], bounds[names[i]][1]) for i in range(len(names))]
+
+        prob.objectives = [jmoo_objective("Effort", True), jmoo_objective("Months", True),
+                           jmoo_objective("Defects", True), jmoo_objective("Risks", True)]
+
+    def evaluate(prob, input=None):
+        if input:
+            for i, decision in enumerate(prob.decisions):
+                decision.value = input[i]
+        else:
+            input = [decision.value for decision in prob.decisions]
+        xomoxo = xomol()
+        output = xomoxo.run(input)
+        for i, objective in enumerate(prob.objectives):
+            objective.value = output[i]
+        return [objective.value for objective in prob.objectives]
+
+    def evalConstraints(prob, input=None):
+        return False  # no constraints
+
+
+class XOMO_allL(jmoo_problem):
+    "Xomo_all large version                                                                 "
+
+    def __init__(prob, percentage=-1):
+        prob.percentage=percentage
+        prob.name = "xomo_all(L)"
+        # Should be as xomol.names to maintain order of LOWs and UPs
+        names = ["aa", "sced", "cplx", "site", "resl", "acap", "etat", "rely",
+                 "Data", "prec", "pmat", "aexp", "flex", "pcon", "tool", "time",
+                 "stor", "docu", "b", "plex", "pcap", "kloc", "ltex", "pr",
+                 "ruse", "team", "pvol"]
+        # Generic Bounds as per menzies.us/pdf/06xomo101.pdf fig.9
+        bounds = {"aa": (1, 6),
+                  "sced": (1.00, 1.43),
+                  "cplx": (0.73, 1.74),
+                  "site": (0.80, 1.22),
+                  "resl": (1.41, 7.07),
+                  "acap": (0.71, 1.42),
+                  "etat": (1, 6),
+                  "rely": (0.82, 1.26),
+                  "Data": (0.90, 1.28),
+                  "prec": (1.24, 6.20),
+                  "pmat": (1.56, 7.80),
+                  "aexp": (0.81, 1.22),
+                  "flex": (1.01, 5.07),
+                  "pcon": (0.81, 1.29),
+                  "tool": (0.78, 1.17),
+                  "time": (1.00, 1.63),
+                  "stor": (1.00, 1.46),
+                  "docu": (0.81, 1.23),
+                  "b": (3, 10),
+                  "plex": (0.85, 1.19),
+                  "pcap": (0.76, 1.34),
+                  "kloc": (2, 1000),
+                  "ltex": (0.84, 1.20),
+                  "pr": (1, 6),
+                  "ruse": (0.95, 1.24),
+                  "team": (1.01, 5.48),
+                  "pvol": (0.87, 1.30)}
+        # bounds specific to all model
+        bounds_all = {"prec": (1.24, 6.2),
+                      "flex": (1.01, 5.07),
+                      "resl": (1.41, 7.07),
+                      "team": (1.01, 5.48),
+                      "pmat": (1.56, 7.8),
+                      "rely": (0.82, 1.26),
+                      "cplx": (0.73, 1.74),
+                      "Data": (0.9, 1.14),
+                      "ruse": (0.95, 1.24),
+                      "time": (1, 1.63),
+                      "stor": (1, 1.17),
+                      "pvol": (0.87, 1.3),
+                      "acap": (0.71, 1.19),
+                      "pcap": (0.76, 1),
+                      "pcon": (0.81, 1.29),
+                      "aexp": (0.81, 1.22),
+                      "plex": (0.91, 1.19),
+                      "ltex": (0.84, 1.2),
+                      "tool": (0.78, 1.09),
+                      "sced": (1, 1.43),
+                      "site": (0.8, 1.22),
+                      "docu": (0.81, 1.23),
+                      "kloc": (7, 418)}
+        # Update main bounds with bounds of all
+        for key, val in bounds_all.items():
+            delta = max(val) - min(val)
+            bounds[key] = (min(val)+delta*0.15, min(val)+delta*0.85)
+
+            if min(val) == max(val): bounds[key] = (min(val), max(val) + 0.000001)  # To remove division by 0
+
+        prob.decisions = [jmoo_decision(names[i], bounds[names[i]][0], bounds[names[i]][1]) for i in range(len(names))]
+
+        prob.objectives = [jmoo_objective("Effort", True), jmoo_objective("Months", True),
+                           jmoo_objective("Defects", True), jmoo_objective("Risks", True)]
+
+    def evaluate(prob, input=None):
+        if input:
+            for i, decision in enumerate(prob.decisions):
+                decision.value = input[i]
+        else:
+            input = [decision.value for decision in prob.decisions]
+        xomoxo = xomol()
+        output = xomoxo.run(input)
+        for i, objective in enumerate(prob.objectives):
+            objective.value = output[i]
+        return [objective.value for objective in prob.objectives]
+
+    def evalConstraints(prob, input=None):
+        return False  # no constraints
