e8293d1ab7327d8b8497aa524181b3a94f441206
Jianfeng <ginfungchan@gmail.com>
2016-10-21 15:38:27 -0400

    --wip--

--- jmoo_hpc.sh
+++ jmoo_hpc.sh
@@ -10,4 +10,4 @@ rm err/*
 #    bsub -W 60 -n 1 -o ./out/out.$i -e ./err/err.%J /share/jchen37/hpc_env/miniconda/bin/python2.7 jmoo_interface.py -repeats 1 -repeatOffset $i
 #done
 
-bsub -W 60 -n 1 -o ./out/out.%J -e ./err/err.%J /share/jchen37/hpc_env/miniconda/bin/python2.7 jmoo_interface.py -chartOnly >> %J.csv
+bsub -W 60 -n 1 -o ./out/out.%J -e ./err/err.%J /share/jchen37/hpc_env/miniconda/bin/python2.7 jmoo_interface.py -chartOnly
