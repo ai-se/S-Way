77444bf4abdf4022c12f6fc6d4f58ea25e0290d8
Jianfeng <ginfungchan@gmail.com>
2016-10-21 15:44:26 -0400

    --wip--

--- jmoo_hpc.sh
+++ jmoo_hpc.sh
@@ -2,7 +2,7 @@
 
 mkdir out
 mkdir err
-rm out/*
+rm out/out.*
 rm err/*
 
 #for i in `seq 0 10`;
@@ -10,4 +10,6 @@ rm err/*
 #    bsub -W 60 -n 1 -o ./out/out.$i -e ./err/err.%J /share/jchen37/hpc_env/miniconda/bin/python2.7 jmoo_interface.py -repeats 1 -repeatOffset $i
 #done
 
-bsub -W 60 -n 1 -o ./out/out.%J -e ./err/err.%J /share/jchen37/hpc_env/miniconda/bin/python2.7 jmoo_interface.py -chartOnly
+
+dt=$(date '+%m%d_%H%M');
+bsub -W 60 -n 1 -o ./out/stats_$dt.%J -e ./err/err.%J /share/jchen37/hpc_env/miniconda/bin/python2.7 jmoo_interface.py -chartOnly
