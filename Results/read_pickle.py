746c049e58b3be1c6ef6060efacd2817641af49e
Jianfeng <ginfungchan@gmail.com>
2016-10-29 21:58:00 -0400

    --wip--

--- Results/read_pickle.py
+++ Results/read_pickle.py
@@ -7,7 +7,7 @@ import numpy as np
 def get_content(pickle_file):
     content = pickle.load(open(pickle_file, "rb"))
     problem_name = content.keys()[-1]
-    print problem_name
+    # print problem_name
     for i in xrange(len(content[problem_name]['Spread'])):
         if content[problem_name]['Spread'][i][0] == 'SWAY5_10000':
             median_score = np.median(content[problem_name]['Spread'][i][1:])
@@ -26,4 +26,12 @@ if __name__ == "__main__":
     # files
     # for f in files:
     #     get_content(f)
-    models = ['POM3A', 'POM3B', 'POM3C', 'POM3D', ]
+    models = ['POM3A', 'POM3B', 'POM3C', 'POM3D', 'xomo_all', 'xomo_flight', 'xomo_ground', 'xomo_osp', 'xomoo2']
+    sizes = ['(S)', '(M)', '(L)', '']
+
+    for m in models:
+        print (m)
+        for s in sizes:
+            get_content('oct29/'+m+s+'.pickle')
+        print ('~\par\n~\par\n')
+        raw_input()
