0264782000066e0ecb35989c5061de9057775d48
Jianfeng <ginfungchan@gmail.com>
2016-10-29 21:32:40 -0400

    --wip--

--- Results/read_pickle.py
+++ Results/read_pickle.py
@@ -3,12 +3,13 @@ from sk import rdivDemo
 import pickle
 import numpy as np
 
+
 def get_content(pickle_file):
     content = pickle.load(open(pickle_file, "rb"))
     problem_name = content.keys()[-1]
     print problem_name
     for i in xrange(len(content[problem_name]['Spread'])):
-        if content[problem_name]['Spread'][i][0] == 'NSGAII_100':
+        if content[problem_name]['Spread'][i][0] == 'SWAY5_10000':
             median_score = np.median(content[problem_name]['Spread'][i][1:])
 
     for ii in xrange(len(content[problem_name]['Spread'])):
@@ -16,10 +17,13 @@ def get_content(pickle_file):
             content[problem_name]['Spread'][ii][j] = (content[problem_name]['Spread'][ii][j] * 100) / median_score
 
     rdivDemo(content[problem_name]["Spread"], globalMinMax=False, isLatex=True)
-    raw_input()
+    # raw_input()
 
 if __name__ == "__main__":
     import os
-    files = [f for f in os.listdir(".") if ".pickle" in f]
-    for f in files:
-        get_content(f)
+    # files = [f for f in os.listdir(".") if ".pickle" in f]
+    # import pdb; pdb.set_trace()
+    # files
+    # for f in files:
+    #     get_content(f)
+    models = ['POM3A', 'POM3B', 'POM3C', 'POM3D', ]
